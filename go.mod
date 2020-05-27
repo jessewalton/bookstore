f7eedbc16448413a6c4377d5319f024143830e3a
Jesse Walton <jesse.m.walton@gmail.com>
2020-05-27 07:56:26 -0600

    Enable CORS

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module bookstore
 go 1.14
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.12
 	github.com/rahmanfadhil/gin-bookstore v0.0.0-20200521154043-2ad4262268f7
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bookstore/controllers"
 	"bookstore/db"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +12,8 @@ func main() {
 	r := gin.Default()
 
 	db.ConnectDataBase()
+
+	r.Use(cors.Default())
 
 	r.GET("/books", controllers.FindBooks)
 	r.POST("/books", controllers.CreateBook)
